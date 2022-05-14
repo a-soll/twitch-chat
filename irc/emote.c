5b052338720be23d1311e5a6c8ad9c6327542cd9
Adam Solloway <ajsolloway@gmail.com>
2022-05-14 11:52:44 -0400

    emote stuff

--- irc/emote.c
+++ irc/emote.c
@@ -17,13 +17,19 @@ void get_global_emotes(Client *client, struct hashmap_s *emote_map) {
     }
 
     for (int i = 0; i < response.data_len; i++) {
-        Emote e;
+        Emote *e = malloc(sizeof(Emote));
+        struct json_object *images;
         response.data_array_obj = json_object_array_get_idx(response.data, i);
-        e.name = get_key(response.data_array_obj, "name");
-        e.id = get_key(response.data_array_obj, "id");
-        if (0 != hashmap_put(emote_map, e.id, strlen(e.id), &e)) {
-            printf("Could not add %s\n", e.name);
+        e->name = get_key(response.data_array_obj, "name");
+        e->id = get_key(response.data_array_obj, "id");
+        images = json_object_object_get(response.data_array_obj, "images");
+        e->url_1x = get_key(images, "url_1x");
+        e->url_2x = get_key(images, "url_2x");
+        e->url_3x = get_key(images, "url_3x");
+        if (0 != hashmap_put(emote_map, e->name, strlen(e->name), e)) {
+            printf("Could not add %s\n", e->name);
         }
+        json_object_put(images);
     }
     clean_response(&response);
 }
@@ -44,6 +50,7 @@ void get_channel_emotes(Client *client, const char *channel_id, struct hashmap_s
         Emote e;
         response.data_array_obj = json_object_array_get_idx(response.data, i);
         e.name = get_key(response.data_array_obj, "name");
+        printf("%s\n", e.name);
         e.id = get_key(response.data_array_obj, "id");
         if (0 != hashmap_put(emote_map, e.name, strlen(e.name), &e)) {
             printf("Could not add %s\n", e.name);
@@ -59,6 +66,35 @@ bool init_emote_map(struct hashmap_s *emote_map, const unsigned initial_size) {
     return true;
 }
 
-void get_emote(const char *emote_id, struct hashmap_s *emote_map) {
-    
+Emote *get_emote(const char *word, struct hashmap_s *emote_map) {
+    Emote *emote = hashmap_get(emote_map, word, strlen(word));
+    return emote;
+}
+
+void parse_emotes(Message *message, struct hashmap_s *emote_map) {
+    Emote *e;
+    int i = 0;
+    int j = 0;
+    char word[MESSAGE_LEN];
+
+    while (message->message[i] != '\0') {
+        if (message->message[i] != ' ') {
+            word[j] = message->message[i];
+            j++;
+        }
+        if (message->message[i] == ' ' || message->message[i + 1] == '\0') {
+            word[j] = '\0';
+            e = get_emote(word, emote_map);
+            if (e) {
+                printf("%s\n", e->name);
+                printf("%s\n", e->id);
+                printf("%s\n", e->url_1x);
+            }
+            j = 0;
+        }
+        i++;
+    }
+}
+
+void get_emote_maybe(const char *word, struct hashmap_s *emote_map) {
 }
--- irc/emote.h
+++ irc/emote.h
@@ -2,12 +2,14 @@
 #define EMOTE_H
 
 #include "client.h"
+#include "irc_struct.h"
 #include "hashmap.h"
-#include <stdbool.h>
 
 typedef struct Emote {
     const char *name;
-    const char *url;
+    const char *url_1x;
+    const char *url_2x;
+    const char *url_3x;
     const char *id;
     int start;
     int end;
@@ -17,6 +19,7 @@ void populate_emote(Emote *emote);
 void get_channel_emotes(Client *client, const char *channel_id, struct hashmap_s *emote_map);
 void get_global_emotes(Client *client, struct hashmap_s *emote_map);
 bool init_emote_map(struct hashmap_s *emote_map, const unsigned initial_size);
-void get_emote(const char *emote_id, struct hashmap_s *emote_map);
+Emote *get_emote(const char *word, struct hashmap_s *emote_map);
+void parse_emotes(Message *message, struct hashmap_s *emote_map);
 
 #endif /* EMOTE_H */
